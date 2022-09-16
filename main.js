8d5c3c0073e42bc233c255541edb501b749f9dd1
01Murod01 <01myoldoshev01@gmail.com>
2022-09-16 21:31:19 +0500

    lesson2

---
+++ main.js
@@ -0,0 +1,39 @@
+// 1 misol
+// let L = prompt("Enter a value ...cm");
+// console.log(L / 100 + "cm");
+
+// 2 misol
+// let M = prompt("Enter a number in kg");
+// console.log(M / 1000 + "t");
+
+// 3 misol
+// topomadm
+
+// a > b;
+// let a = Math.floor(Math.random() * 10);
+// let b = Math.floor(Math.random() * 10);
+// console.log(a, b);
+
+// 4 misol
+// let ab = Math.floor(Math.random() * 10);
+// console.log(ab);
+// console.log(a);
+
+// 5 misol
+// let a = Math.floor(Math.random() * 100);
+// let b = Math.floor(Math.random() * 100);
+// console.log(a + b);
+
+// 6 misol
+// let a = Math.floor(Math.random() * 100);
+// let b = Math.floor(Math.random() * 100);
+// console.log(a * b);
+
+// 7 misol
+// topomadm
+
+// 8 misol
+// let N = prompt(
+//   "Oradan kiritgan vaqtingizchalik sekund o'tdi va javobida o'sha vaqt necha soat necha daqiqaligi chiqadi"
+// );
+// console.log(N / 60 / 60 + "soat" , N / 60 + "daqiqa");
